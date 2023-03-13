1c41e287be40a1252ead6b94cfaf987c52e70ca5
Lilmanlogy <89665049+Lilmanlogy@users.noreply.github.com>
2023-03-12 22:49:55 -0400

    Player Fix

--- HollowCrest/Assets/Scripts/SwingScript.cs
+++ HollowCrest/Assets/Scripts/SwingScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SwingScript : MonoBehaviour
 {
+    public PlayerMovement Player;
     // Start is called before the first frame update
+  
+    
     void Start()
     {
-        
+        Player = FindObjectOfType<PlayerMovement>();
+       
+            if (Player.Right == true)
+            {
+                
+                gameObject.transform.localScale = new Vector3(.3f, .3f, .3f);
+            }
+            if (Player.Right == false)
+            {
+                
+                gameObject.transform.localScale = new Vector3(-.3f, .3f, .3f);
+            }
+        StartCoroutine(waiter());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator waiter()
     {
-        
+        yield return new WaitForSeconds(.2f);
+        Destroy(gameObject);
     }
 }
+
+    // Update is called once per frame
+    
+        
+ 
+
